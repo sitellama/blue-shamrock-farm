af154504f6631c757d40885636f7c147d384401b
maxzakh <maks.zakh@gmail.com>
2024-09-18 20:59:29 -0400

    fix: add alt text to animals page 2

--- src/components/4-library/support-us.tsx
+++ src/components/4-library/support-us.tsx
@@ -12,46 +12,38 @@ export function SupportUs() {
 
             <div className="w-[95%] max-w-[1200px] mx-auto flex flex-col md:flex-row items-center md:items-start justify-center gap-[5%]">
                 <div className="flex flex-col items-center md:flex-[33%] mt-8">
-                    <img src={cow} className="rounded-full border-solid border-4 border-white w-[292px]" />
-                    <a href="https://www.amazon.com/hz/wishlist/ls/1KF35XP0KD1RI?ref_=wl_share" target="_blank" className="btn-a mx-auto text-center text-2xl">Amazon Wish List</a>
+                    <img src={cow} className="rounded-full border-solid border-4 border-white w-[292px]" alt="zoomed in snoot of a cow" />
+                    <a
+                        href="https://www.amazon.com/hz/wishlist/ls/1KF35XP0KD1RI?ref_=wl_share"
+                        target="_blank"
+                        className="btn-a mx-auto text-center text-2xl"
+                    >
+                        Amazon Wish List
+                    </a>
                 </div>
 
                 <div className="flex flex-col items-center md:flex-[33%] mt-8">
-                    <img src={soap} className="rounded-full border-solid border-4 border-white w-[292px]" />
-                    <a href="https://blue-shamrock-farm-llc.square.site/" target="_blank" className="btn-a mx-auto text-center text-2xl">Blue Shamrock Shop</a>
+                    <img src={soap} className="rounded-full border-solid border-4 border-white w-[292px]" alt="colorful blocks of soap" />
+                    <a
+                        href="https://blue-shamrock-farm-llc.square.site/"
+                        target="_blank"
+                        className="btn-a mx-auto text-center text-2xl"
+                    >
+                        Blue Shamrock Shop
+                    </a>
                 </div>
 
                 <div className="flex flex-col items-center md:flex-[33%] mt-8">
-                    <img src={pig} className="rounded-full border-solid border-4 border-white w-[292px]" />
-                    <a href="#" target="_blank" className="btn-a mx-auto text-center text-2xl">Useful Items to Donate</a>
+                    <img src={pig} className="rounded-full border-solid border-4 border-white w-[292px]" alt="little white and brown pigs eating hay" />
+                    <a
+                        href="#"
+                        target="_blank"
+                        className="btn-a mx-auto text-center text-2xl"
+                    >
+                        Useful Items to Donate
+                    </a>
                 </div>
             </div>
-
-            {/* <div className="flex flex-col md:flex-row items-center justify-center max-w-[1000px] mx-auto h-[268px]">
-                <div className="flex flex-1 justify-center items-center">
-                    <img src={cow} className=" rounded-full h-[300px] border-solid border-4 border-white" />
-                </div>
-
-                <div className="flex-1 px-4 h-full">
-                    <div className="w-[600px] mx-auto flex flex-col justify-between h-full">
-                        <div className="btn-outer bg-white w-full">
-                            <div className="btn-inner border-black w-[100%] flex items-center md:order-last">
-                                <a href="#" className="btn-a text-black mx-auto">Amazon Animal Wish List</a>
-                            </div>
-                        </div>
-                        <div className="btn-outer bg-white w-full">
-                            <div className="btn-inner border-black w-[100%] flex items-center">
-                                <a href="#" className="btn-a text-black mx-auto">Blue Shamrock Shop</a>
-                            </div>
-                        </div>
-                        <div className="btn-outer bg-white w-full">
-                            <div className="btn-inner border-black w-[100%] flex items-center">
-                                <a href="#" className="btn-a text-black mx-auto">Useful Items to Donate</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </section>
     );
 }
