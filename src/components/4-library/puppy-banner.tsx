5e818f22cd1bd26f1fac1f96ce7d425d8122853f
Karina Rogers <krogers@ucdavis.edu>
2025-08-08 23:52:36 -0400

    fix: typo

--- src/components/4-library/puppy-banner.tsx
+++ src/components/4-library/puppy-banner.tsx
@@ -9,7 +9,7 @@ export function PuppyBanner({ pagelink, announcement }: { pagelink: boolean; ann
             <div className="max-content sm:w-[40rem] py-4 sm:py-8">
                 <img src={icon} alt="" className="h-20 mx-auto" />
                 <h1 className="text-5xl">We're expecting puppies !</h1>
-                <p>Blue Shamrock Farm has some wonderful news. Pluto and Nika are excepting an exceptional litter due August 2025.</p>
+                <p>Blue Shamrock Farm has some wonderful news. Pluto and Nika are expecting an exceptional litter due August 2025.</p>
                 {pagelink && <p><a href="/tornjak">MEET THE PARENTS </a></p>}
                 {announcement && <img src={announcementImg} className="mx-auto" alt="litter announcement: Sire is Pluto Dalmatinski Vrisak. Dam is Sara-unique Herceg-bos-tor. Puppies expected August 2025" />}
                 <p>Please reach out via <a href="https://www.facebook.com/blueshamrockfarm">facebook</a> or our <a href="#contact">contact form</a> for more information.</p>
