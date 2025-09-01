a0e586f74dc2fe28db9181e18e0c18be65e368ca
Karina Rogers <krogers@ucdavis.edu>
2025-09-01 19:38:35 -0400

    fix: typos

--- src/components/4-library/our-tornjaks.tsx
+++ src/components/4-library/our-tornjaks.tsx
@@ -10,7 +10,7 @@ export function OurTornjaks() {
             <section className="max-content text-center">
                 <div>
                     <h2>Our Tornjaks</h2>
-                    <p >The Tornjak is a livestock guardian dog originating from Bosnia and Herzegovina and Croatia. The task of protecting all of Blue Shamrock Farm's precious animals falls to our Tornjaks. Whether its in on the farm or at the show ring, we are always proud to introduce our beautiful working dogs!</p>
+                    <p >The Tornjak is a livestock guardian dog originating from Bosnia and Herzegovina and Croatia. The task of protecting all of Blue Shamrock Farm's precious animals falls to our Tornjaks. Whether it is on the farm or at the show ring, we are always proud to introduce our beautiful working dogs!</p>
                     <hr className="h-1 border-0 bg-blue-dark  max-content mt-16 mx-auto"></hr>
                 </div>
             </section>
@@ -24,7 +24,7 @@ export function OurTornjaks() {
 
                         <div>
                             <h2>Pluto </h2>
-                            <p >Pluto's offical name is Pluto Dalmatinski Vrisak. He is a vigilant but gentle guardian for his animals. He is constantly checking his flock to make sure everyone is healthy. He helps ensure all of the babies get appropriate food by blocking some of the more assertive animals. Pluto also alerts me to predators but is able to handle situations by himself if needed.</p>
+                            <p >Pluto's offical name is Pluto Dalmatinski Vrisak. He is a vigilant but gentle guardian for his animals. He is constantly checking his flock to make sure everyone is healthy. He helps ensure that all of the babies get appropriate food by blocking some of the more assertive animals. Pluto also alerts me to predators but is able to handle situations by himself if needed.</p>
                             <p><a href="https://tornjak.pedigre.net/details.php?id=6513" target="_blank">Pedigree</a></p>
                             <p><a href="https://ofa.org/advanced-search/?appnum=2584059" target="_blank">OFA testing</a></p>
                         </div>
@@ -37,7 +37,7 @@ export function OurTornjaks() {
                     <div className="grid grid-cols-1 md:items-center gap-y-8 md:grid-cols-2 md:gap-x-16">
                         <div className="order-2 md:order-1">
                             <h2>Nika</h2>
-                            <p >Nika's offical name is Sara-unique Herceg-bos-tor. She was the very first tornjak to enter a dog show in the USA. Although she loves and protects all her animals, Nika has a soft spot for the tiny bunnies and guinea pigs. She counts them regularly to make sure everyone is accounted for. </p>
+                            <p >Nika's offical name is Sara-unique Herceg-bos-tor. She was the very first Tornjak to enter a dog show in the USA. Although she loves and protects all her animals, Nika has a soft spot for the tiny bunnies and guinea pigs. She counts them regularly to make sure everyone is accounted for. </p>
                             <p><a href="https://tornjak.pedigre.net/details.php?id=6513" target="_blank">Pedigree</a></p>
                             <p><a href="https://ofa.org/advanced-search/?appnum=2584057" target="_blank">OFA testing</a></p>
                         </div>
@@ -58,7 +58,7 @@ export function OurTornjaks() {
 
                         <div>
                             <h2>Ariela </h2>
-                            <p >Ariela's offical name is Ariela Hrvatska Dika. She isa big fan of water and loves to help with the chores.She is comfortable around horses, ponies, and miniature horses.</p>
+                            <p >Ariela's offical name is Ariela Hrvatska Dika. She is a big fan of water and loves to help with the chores. She is comfortable around horses, ponies, and miniature horses.</p>
                             <p><a href="https://tornjak.pedigre.net/en/details.php?id=6556 " target="_blank">Pedigree</a></p>
                             <p>OFA testing - Not old enough yet</p>
                         </div>
@@ -68,7 +68,7 @@ export function OurTornjaks() {
 
             <div className="btn-outer bg-blue-dark ">
                 <div className="btn-inner">
-                    <a href="https://sites.google.com/view/blueshamrockfarm/tornjak" target="_blank" className="btn-a">Full Tornjak Gallery</a>
+                    <a href="https://sites.google.com/view/blueshamrockfarm/tornjak" target="_blank" className="btn-a">More Tornjak Details</a>
                 </div>
             </div>
             <hr className="h-1 border-0 bg-blue-dark  max-content mt-16 mx-auto"></hr>
